defc3cdad1dc92b1eb08500fc403a2b12cbdb1c8
Josse29 <pinemjosse@gmail.com>
2025-06-23 17:42:21 +0700

    updating

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/portfolio/", // ← harus sesuai nama repo kamu
+  plugins: [react()],
 });
